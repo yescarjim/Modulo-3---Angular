1db6c8b28abd8043b4df18e6f3d8fc6a7da239e3
yescarjim <yesmykarito@gmail.com>
2025-10-16 22:21:10 -0500

    Formularios y configuracion inicio Sesion

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+// Formularios reactivos -> cada cosa que el usuario escriba sea reconocido por el sistema
+import { ReactiveFormsModule, FormControl, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
 
+  loginForm = new FormGroup({
+    emailLogin: new FormControl(""),
+    passwordLogin: new FormControl(""),
+  })
+
+  
+  //manejo de eventos
+  handleSubmit() {
+  const email = this.loginForm.value.emailLogin
+  const password = this.loginForm.value.passwordLogin
+  console.log(email,password)
+
+    //const credencials: Credencials = {
+    //  emailLogin: this.loginForm.value.emailLogin,
+   //   passwordLogin: this.loginForm.value.passwordLogin
+  //  }
+
+  //  console.log("Credenciales para Login", credencials);
+    //Logica de la petición al back inicio de Sesion
+  }
 }
--- src/app/pages/register/register.ts
+++ src/app/pages/register/register.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormControl, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.html',
   styleUrl: './register.css'
 })
 export class Register {
+registerForm = new FormGroup ({
+  name : new FormControl (""),
+  username : new FormControl (""),
+  email : new FormControl(""),
+  age : new FormControl <number|null >(null),
+  password : new FormControl <string>("")
+})
 
 }
